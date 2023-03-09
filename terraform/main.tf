de9b6863384343444b2565f4c7dabb0508b1a569
shkumarpradhan <127394404+shkumarpradhan@users.noreply.github.com>
2023-03-09 18:12:09 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,27 @@
 resource "aws_s3_bucket" "DevopsIacPipeline" {
   bucket = "devopspipelinebkt"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_object" "index" {
+  bucket       = "${aws_s3_bucket.DevopsIacPipeline.bucket}"
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+  etag         = "${md5(file("index.html"))}"
+  acl          = "public-read"
+}
+
+resource "aws_s3_bucket_object" "error" {
+  bucket       = "${aws_s3_bucket.DevopsIacPipeline.bucket}"
+  key          = "error.html"
+  source       = "error.html"
+  content_type = "text/html"
+  etag         = "${md5(file("error.html"))}"
+  acl          = "public-read"
 }
