8b12a06cfca3c42b59f0dc581b2bbe0f87339f39
Jinsy05 <110526639+Jinsy05@users.noreply.github.com>
2023-03-09 15:07:55 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,11 @@
 provider "aws" {
-    assume_role {
-        role_arn = "arn:aws:iam::820912175766:user/iac_deployment_user"
-    }
-    region = var.region
+  assume_role {
+    role_arn     = "arn:aws:iam::123456789012:role/ROLE_NAME"
+    session_name = "SESSION_NAME"
+    external_id  = "EXTERNAL_ID"
+   }
+  region = var.region
   }
+
+
 
