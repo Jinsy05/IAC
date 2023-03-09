d41d2317abdb9f94240fdc70d3772b6b9a8ec27f
Jinsy05 <110526639+Jinsy05@users.noreply.github.com>
2023-03-09 14:54:04 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,8 @@
 provider "aws" {
-  assume_role {
-    role_arn     = "arn:aws:iam::820912175766:role/S3"
-    session_name = "S3"
+    assume_role {
+        role_arn = "arn:aws:iam::820912175766:user/iac_deployment_user"
+        profile = "default"
+    }
+    region = var.region
   }
 }
